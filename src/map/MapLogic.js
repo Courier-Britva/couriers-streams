f0002c7f0534e0125e0d5c2230050cb639975ddb
Courier <artar2004@gmail.com>
2025-04-15 05:48:08 +0400

    first commit

---
+++ src/map/MapLogic.js
@@ -0,0 +1,58 @@
+import { ComposableMap, Geographies, Geography, Marker, ZoomableGroup } from 'react-simple-maps'
+import mapData from './../mapdata.json'
+
+const geoUrl = '/countries-110m.json'
+
+export default function MapWithInteraction() {
+  return (
+    <ComposableMap
+      projection="geoEqualEarth"
+      className='svg__body'
+    >
+      <ZoomableGroup
+        center={[10.0, 50.0]}
+        zoom={4}
+        minZoom={3.2}
+        maxZoom={6}
+        zoomStep={0.05}
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#222"
+                stroke="#444"
+                strokeWidth={0.4}
+                style={{
+                  default: { outline: 'none' },
+                  hover: { outline: 'none' },
+                  pressed: { outline: 'none' }
+                }}
+              />
+            ))
+          }
+        </Geographies>
+
+        {mapData.map((point, index) => (
+          <Marker key={index} coordinates={[point.lng, point.lat]}>
+            <foreignObject width={38} height={23} x={0} y={0}>
+              <a
+                href={point.videoUrl}
+                xmlns="http://www.w3.org/1999/xhtml"
+                className="map__element__container"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={point.thumbnail} className="map__element__preview" alt={point.title} />
+                <span className="map__element_name">{point.title}</span>
+                <span className="map__element__years">{point.years}</span>
+              </a>
+            </foreignObject>
+          </Marker>
+        ))}
+      </ZoomableGroup>
+    </ComposableMap>
+  )
+}
